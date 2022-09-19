ff1e52cb8e97a4ee074df08c764f6e11979a3610
LukeOnCode <LukasPetrylaEmail@gmail.com>
2022-09-19 19:09:05 +0200

    working with objects

--- exercise/index_4.js
+++ exercise/index_4.js
@@ -21,8 +21,8 @@ const h1 = document.querySelector('h1');
 h1.innerHTML = '';
 const output = document.querySelector('.output');
 const inputVal = document.querySelector('.val');
-inputVal.value = 'Learn to Code';
-btn.textContent = 'Click Me now';
+inputVal.value = '';
+btn.textContent = 'Search by term';
 
 btn.addEventListener('click', (e) => {
     e.preventDefault();
@@ -54,23 +54,37 @@ function maker(arr, category) {
     let input_value = inputVal.value;
     arr[category].forEach(el => {
         const div = document.createElement('div');
+        let br = document.createElement('br');
         div.classList.add('box');
         output.append(div);
+        output.append(br);
         const entries = Object.entries(el);
-        console.log(el.title);
-
+        
         if(Object.values(el).includes(input_value)){
             div.innerHTML = 'Properties : ' + entries.length;
             for (const obj of entries) {
                 div.innerHTML += `<br> ${obj[0]} : ${obj[1]}`;
             }   
         }
+
         if(!input_value){
             div.innerHTML = 'Properties : ' + entries.length;
             for (const obj of entries) {
                 div.innerHTML += `<br> ${obj[0]} : ${obj[1]}`;
-            }  
+                if(obj[0] === 'platform' && obj[1] !== null){
+                    console.log(obj[1]);
+                }
+                 /*   if(obj[1] != null && obj[1].hasOwnProperty('platform')){
+                        var nested_obj = obj[1].platform;
+                        console.log(nested_obj)
+                        div.innerHTML += `<br> ${nested_obj} : `
+                        for(const very of nested_obj){
+                            console.log(very[0] + very[1])
+                        }
+                        console.log(nested_obj)
+                        //div.innerHTML += `<br> ${nested_obj} : `
+                    } */    
+            }            
         }
-
     });
 }
