308e53d5d737128278badaf5e24b20d89d59fd1b
lukeoncode <lukaspetrylaemail@gmail.com>
2023-09-10 19:28:12 +0200

    change template and javascript exercise 4

--- exercise/index_4.js
+++ exercise/index_4.js
@@ -1,4 +1,3 @@
-const btn = document.querySelector('.btn');
 const urls = [
     {
         'url': 'http://localhost:5000/api/exercise_2',
@@ -17,41 +16,114 @@ const urls = [
     }
 ];
 
-const h1 = document.querySelector('h1');
-h1.innerHTML = '';
-const output = document.querySelector('.output');
-const inputVal = document.querySelector('.val');
-inputVal.value = '';
-btn.textContent = 'Search by term';
+const searchWord= document.querySelector('.search-word');
+const output    = document.querySelector('.output');
+const btnBook   = document.querySelector('.btn-book');
+const btnFriend = document.querySelector('.btn-friend');
+const btnCrypto = document.querySelector('.btn-crypto');
+const btnSearch = document.querySelector('.btn-search');
 
-btn.addEventListener('click', (e) => {
+btnSearch.addEventListener('click', e => {
     e.preventDefault();
-    urls.forEach(item => {myURL(item)})
+    let value = searchWord.value;
+    urls.forEach(element => searchUrl(element, element.url, value));
 })
 
-urls.forEach((ele) => {
-    const btn1 = document.createElement('button');
-    btn1.classList.add('btn');
-    h1.append(btn1);
-    btn1.textContent = ele.title;
-    btn1.addEventListener('click', (e) => {
-        myURL(ele);
-    })
-})
-
-function myURL(myObj) {
-    let url = myObj.url;
+const searchUrl = (element, url, value) => {
     fetch(url)
-    .then(rep => rep.json())
-    .then( (data) => {
-        const json = data[0];
-        output.innerHTML += url + myObj.arr + '<br>';
-        maker(json, myObj.arr);
-    })
+    .then(res => res.json())
+    .then(data => {
+        const finalData = data[Number(Object.keys(data))]
+        let category = element.arr;
+        let searchedValue = value;
+        createNode(finalData, category, searchedValue);
+    }).catch(err => console.log(err));
 }
 
+const createNode = (data, objectName, value) => {
+    //fragment
+    const fragment = document.createDocumentFragment();
+
+    const createParent = (a, b, c, d, e, f, g, h ) => {
+        const parentDiv = document.createElement('div')
+        parentDiv.classList.add('parent-list')
+        parentDiv.append(a, b, c)
+        output.append(parentDiv)
+    }    
+    //book
+    let titleText;
+    let authorText;
+    let ISBNText;
+
+    //people
+    let nameText;
+    let lastNameText;
+    
+    //crypto
+    let cryptoNameText;
+    let cryptoSymbolText;
+    let cryptoDataText;
+
+    data[objectName].forEach(element => {
+        const book = element => {
+            //book
+            const bookTitlecomponent  = document.createElement('div');
+            const bookAuthorcomponent = document.createElement('div');
+            const bookISBNcomponent   = document.createElement('div');
+
+            titleText  = document.createTextNode(`${element.title }`)
+            authorText = document.createTextNode(`${element.author}`)
+            ISBNText   = document.createTextNode(`${element.isbn  }`)
+            
+            //user
+            const peopleNamecomponent     = document.createElement('div');
+            const peopleLastNamecomponent = document.createElement('div');
+            
+            nameText = document.createTextNode(`${element.first}`)
+            lastNameText = document.createTextNode(`${element.last}`)
+           
+            //crypto
+            const cryptoNameComponent = document.createElement('div');
+            const cryptoSymbolComponent = document.createElement('div');
+            const cryptoDataComponent = document.createElement('div');
+        
+            cryptoNameText = document.createTextNode(`${element.name}`);
+            cryptoSymbolText = document.createTextNode(`${element.symbol}`);
+            cryptoDataText = document.createTextNode(`${element.first_historical_data}`);
+        
+       
+            //add text 
+            bookTitlecomponent.appendChild(titleText)
+            bookAuthorcomponent.appendChild(authorText)
+            bookISBNcomponent.appendChild(ISBNText)
+            
+            peopleNamecomponent.appendChild(nameText)
+            peopleLastNamecomponent.appendChild(lastNameText)
+
+            cryptoNameComponent.appendChild(cryptoNameText)
+            cryptoSymbolComponent.appendChild(cryptoSymbolText)
+            cryptoDataComponent.appendChild(cryptoDataText)
+  
+            switch (data[objectName]) {
+                case data.books:
+                    createParent(bookTitlecomponent, bookAuthorcomponent, bookISBNcomponent)
+                    break;
+                case data.crypto:
+                    createParent(cryptoNameComponent, cryptoSymbolComponent, cryptoDataComponent)
+                    break;
+                case data.people:
+                    createParent(peopleNamecomponent, peopleLastNamecomponent)
+                    break;
+                default:
+                    break;
+            }
+        }
+        book(element);
+    })
+}
+/*
 function maker(arr, category) {
-    let input_value = inputVal.value;
+    let input_value = searchWord.value;
     console.log(arr[category]);
     arr[category].forEach(el => {
         const div = document.createElement('div');
@@ -84,3 +156,18 @@ function maker(arr, category) {
         }
     });
 }
+// btn.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     urls.forEach(item => {myURL(item)})
+// })
+// urls.forEach((ele) => {
+//     const btn1 = document.createElement('button');
+//     btn1.classList.add('btn');
+//     h1.append(btn1);
+//     btn1.textContent = ele.title;
+//     btn1.addEventListener('click', (e) => {
+//         myURL(ele);
+//     })
+// })
+*/
+
