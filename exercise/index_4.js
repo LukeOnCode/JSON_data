1b52bbbc094b5da27b28961117fbf980fa681536
lukeoncode <lukaspetrylaemail@gmail.com>
2023-09-13 01:44:48 +0200

    edit variable name, add changes for selecting json and category

--- exercise/index_4.js
+++ exercise/index_4.js
@@ -19,37 +19,50 @@ const urls = [
 const searchWord= document.querySelector('.search-word');
 const output    = document.querySelector('.output');
 const btnBook   = document.querySelector('.btn-book');
-const btnFriend = document.querySelector('.btn-friend');
+const btnFriend = document.querySelector('.btn-people');
 const btnCrypto = document.querySelector('.btn-crypto');
 const btnSearch = document.querySelector('.btn-search');
 
+let searchAll;
 //fragment
 const fragment = document.createDocumentFragment();
 
-const thereIs = (keyword, exist) => {
-    if(keyword.length === 0 && exist){
+btnBook.addEventListener('click', e =>{
+    e.preventDefault();
+    e.target.classList.toggle('requested');
+})
+btnFriend.addEventListener('click', e =>{
+    e.preventDefault();
+    e.target.classList.toggle('requested');
+})
+btnCrypto.addEventListener('click', e =>{
+    e.preventDefault();
+    e.target.classList.toggle('requested');
+})
+
+const noKeywordQuery = (keyword, queryAll) => {
+    if(keyword.length === 0 && queryAll){
         urls.forEach(el => searchUrl(el.url, el.arr, keyword) )
     }
 }
 
 btnSearch.addEventListener('click', e => {
+    let keyword = searchWord.value;
+    searchAll = true;
     e.preventDefault();
-    let keyword = searchWord.value.toLowerCase();
-
-    let exist = false;
     [btnBook, btnCrypto, btnFriend].map((el) => {
-        if( el.className.includes('requested')){
-            exist = false;
+        if(el.className.includes('requested')){
             let requestedUrl = el.id;
             let iterator = urls.values();
             for (const value of iterator) {
                 if (value.arr == requestedUrl){
-                    searchUrl(value.url, value.arr, keyword)
-
+                    searchAll = false;
+                    searchUrl(value.url, value.arr, keyword);
                 }
             }
         }
     })
+    noKeywordQuery(keyword, searchAll);
 });
 
 
@@ -65,6 +78,7 @@ const searchUrl = (objectUrl, requestedUrl, value) => {
 }
 
 const createNode = (data, objectName, value) => {
+    
     const createParent = (...array) => {
         const parentDiv = document.createElement('div')
         parentDiv.classList.add('parent-list')
@@ -90,7 +104,7 @@ const createNode = (data, objectName, value) => {
     let cryptoDataText;
     data[objectName].forEach(object => {
 
-        const book = element => {
+        const createChild = element => {
 
             //book
             const bookTitlecomponent  = document.createElement('div');
@@ -145,16 +159,16 @@ const createNode = (data, objectName, value) => {
                     break;
             }
         }
-        if(value.length === 0){
-            book(object)
-        }
-        let counter = 0;        
-        Object.values(object).forEach(el => {
-            if(typeof el == "string" && el.toLowerCase() === value ){ 
-                counter++;
-                counter <= 1 ? book(object) : counter = 0;
+            if(value.length === 0){
+                createChild(object)
             }
-        })
+            let counter = 0;        
+            Object.values(object).forEach(el => {
+                if(typeof el == "string" && el.toLowerCase() === value ){ 
+                    counter++;
+                    counter <= 1 ? createChild(object) : counter = 0;
+                }
+            })
     })
 
 output.appendChild(fragment);
